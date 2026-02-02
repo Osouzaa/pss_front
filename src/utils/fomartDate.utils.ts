be3770b5651596fad65ebbf5748081f713a0ef87
Osouzaa <gabrielsouzaalves750@gmail.com>
2026-02-02 11:52:21 -0300

    feat: ajsutes data

--- src/utils/fomartDate.utils.ts
+++ src/utils/fomartDate.utils.ts
@@ -1,14 +1,21 @@
-import { format, parseISO, isValid } from "date-fns";
+import { format, isValid } from "date-fns";
 import { ptBR } from "date-fns/locale";
 
 export function formatDate(
   date?: string | Date | null,
-  pattern: string = "dd/MM/yyyy"
+  pattern: string = "dd/MM/yyyy",
 ): string {
   if (!date) return "—";
 
-  const parsed =
-    typeof date === "string" ? parseISO(date) : date;
+  let parsed: Date;
+
+  if (typeof date === "string") {
+    // 🔥 evita conversão UTC
+    const [year, month, day] = date.split("T")[0].split("-").map(Number);
+    parsed = new Date(year, month - 1, day);
+  } else {
+    parsed = date;
+  }
 
   if (!isValid(parsed)) return "—";
 
